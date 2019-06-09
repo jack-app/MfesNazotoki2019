982e775252a31e2289937e7a88e18999867d9b05
yunine16 <yuchin616@icloud.com>
2019-06-09 14:14:19 +0900

    fix for UI

---
+++ MfesNazotoki2019/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    GameObject inputField;
+    InputManager InputManager;
+    Transform parentTransform;
+    public GameObject RoomNode;
+    public GameObject Content;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GameObject.Find("InputField");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        InputManager = inputField.GetComponent<InputManager>();
+        InputManager.InputLogger();
+        GameObject prefab = Instantiate(RoomNode);
+        prefab.transform.parent = Content.transform;
+
+
+        string inputText = InputManager.inputValue;
+        Debug.Log(inputText);
+
+        prefab.transform.Find("Text").GetComponent<Text>().text = inputText;
+    }
+}
---
+++ MfesNazotoki2019/Assets/Scripts/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    InputField inputField;
+    public string inputValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+        InitInputField();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InputLogger()
+    {
+        inputValue = inputField.text;
+        //Debug.Log(inputValue);
+        InitInputField();
+    }
+
+
+    public void InitInputField()
+    {
+        //値をリセット
+        inputField.text = "";
+
+        //フォーカス
+        inputField.ActivateInputField();
+    }
+}
