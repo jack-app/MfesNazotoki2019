75286cbb6941f9f0a4bb7e77531c4db4150b3ec0
yunine16 <yuchin616@icloud.com>
2019-06-11 13:36:47 +0900

    add Main Scene

--- MfesNazotoki2019/Assets/Scripts/ButtonController.cs
+++ MfesNazotoki2019/Assets/Scripts/ButtonController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour
 {
     GameObject inputField;
+    GameObject scrollView;
     InputManager InputManager;
     Transform parentTransform;
     public GameObject RoomNode;
@@ -16,6 +17,9 @@ public class ButtonController : MonoBehaviour
     void Start()
     {
         inputField = GameObject.Find("InputField");
+        scrollView = GameObject.Find("Scroll View");
+
+
 
     }
 
@@ -27,15 +31,36 @@ public class ButtonController : MonoBehaviour
 
     public void OnClick()
     {
+        //Scroll Viewのサイズの取得
+        RectTransform rectTransform = scrollView.GetComponent<RectTransform>();
+        //Debug.Log(rectTransform.rect.width);
+        //Debug.Log(rectTransform.rect.height);
+        float width = rectTransform.rect.width;
+        float height = rectTransform.rect.height;
+        //Debug.Log(width);
+        //Debug.Log(height);
+
+
+        //入力内容の取得
         InputManager = inputField.GetComponent<InputManager>();
         InputManager.InputLogger();
-        GameObject prefab = Instantiate(RoomNode);
-        prefab.transform.parent = Content.transform;
-
 
         string inputText = InputManager.inputValue;
-        Debug.Log(inputText);
+        //Debug.Log(inputText);
 
-        prefab.transform.Find("Text").GetComponent<Text>().text = inputText;
+
+        //コメントが入力されていたら表示
+        if (inputText != "")
+        {
+            GameObject prefab = Instantiate(RoomNode);
+            prefab.transform.parent = Content.transform;
+
+            //RoomNodeのサイズ調整
+            prefab.GetComponent<RectTransform>().localScale = new Vector3((float)1.3, (float)1.3, 1);                              
+
+
+            prefab.transform.Find("Text").GetComponent<Text>().text = inputText;
+        }
+        
     }
 }
