65c212918d9e61b8c1cbcf2740d778ae290a32d9
watson70percent <38928314+watson70percent@users.noreply.github.com>
2019-06-12 23:48:52 +0900

    update AnswerCheckScript

--- MfesNazotoki2019/Assets/Scripts/AnswerCheckScript.cs
+++ MfesNazotoki2019/Assets/Scripts/AnswerCheckScript.cs
@@ -1,11 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
+
+
 
 public class AnswerCheckScript : MonoBehaviour
 {
 
+    //Inspectorに複数データを表示するためのクラス
+    
+    [System.SerializableAttribute]
+    public class ValueList
+    {
+        public List<string> List = new List<string>();
+
+        public ValueList(List<string> list)
+        {
+            List = list;
+        }
+    }
+    
+   // [SerializeField]
+   // private List<int> _valueList;
+
+    //Inspectorに表示される
+    [SerializeField]
+    private List<ValueList> correctAnswerList = new List<ValueList>();
+
+
     public string answer;
+   
+   
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +43,40 @@ public class AnswerCheckScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int questionNumber = 1;
+        int c = Check(questionNumber, answer);
+        Debug.Log(c);
     }
 
-    void Check(string answer, string correctAnswer)
-    {
 
+    //引数は　問題番号(0スタート), 入力したこたえ
+    public int Check(int questionNumber, string answer)
+    {
+        //わからないと聞かれたら-1を返す
+        if(Regex.Match(answer, "わからない").Success)
+        {
+            return -1;
+        }
+        //correctAnswerList[questionNumber] = "a";
+        for (int i = 0;i < correctAnswerList[questionNumber].List.Count; i++)
+        {
+
+            Match match = Regex.Match(answer, correctAnswerList[questionNumber].List[i]);
+
+            if (match.Success)
+            {
+                //合ってたら1を返す
+                return 1;
+            }
+
+            /*
+            if(answer == correctAnswers[i])
+            {
+                return true;
+            }*/
+        }
+        //間違ってたら0を返す
+        return 0;
     }
 }
+
